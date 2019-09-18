6fa58a52269018f8d4cbcd8c264ef05d009672d5
Alvis Hsu <junchoon14@gmail.com>
2019-09-19 00:21:59 +0800

    Generate seed file

---
+++ seeders/20190918161947-my-seed-file.js
@@ -0,0 +1,112 @@
+'use strict'
+const bcrypt = require('bcrypt-nodejs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'root@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'admin',
+          name: 'root',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user1@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'user',
+          name: 'user1',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user2@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'user',
+          name: 'user2',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+
+    queryInterface.bulkInsert(
+      'Tweets',
+      Array.from({ length: 50 }).map((d) => ({
+        description: faker.lorem.text(),
+        UserId: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    queryInterface.bulkInsert(
+      'Replies',
+      Array.from({ length: 20 }).map((d) => ({
+        comment: faker.lorem.text(),
+        UserId: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        TweetId: faker.random.number({
+          min: 1,
+          max: 50
+        }),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    queryInterface.bulkInsert(
+      'Likes',
+      Array.from({ length: 20 }).map((d) => ({
+        UserId: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        TweetId: faker.random.number({
+          min: 1,
+          max: 50
+        }),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    return queryInterface.bulkInsert(
+      'Followships',
+      Array.from({ length: 20 }).map((d) => ({
+        FollowingId: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        FollowerId: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Tweets', null, {})
+    queryInterface.bulkDelete('Replies', null, {})
+    queryInterface.bulkDelete('Likes', null, {})
+    return queryInterface.bulkDelete('Followships', null, {})
+  }
+}
