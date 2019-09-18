1ba640da938f5d4e3f7f5e6dc6b66a78d45bffe0
Alvis Hsu <junchoon14@gmail.com>
2019-09-19 00:11:05 +0800

    Bulid Model User

--- models/user.js
+++ models/user.js
@@ -1,8 +1,33 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  )
   User.associate = function(models) {
-  };
-  return User;
-};
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
+    User.hasMany(models.Like)
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+  }
+  return User
+}
